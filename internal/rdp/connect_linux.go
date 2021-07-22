674520649e7c96c0315171e5937ee3d151accb52
danhale-git <danhale87@gmail.com>
2021-07-22 09:58:55 +0100

    Fix linux Connect function.

--- internal/rdp/connect_linux.go
+++ internal/rdp/connect_linux.go
@@ -1,6 +1,6 @@
 package rdp
 
 // Connect is included here to avoid compile errors when CI runs unit tests on linux
-func Connect(rdp *RDP) error {
+func Connect(rdp *RDP, debug bool) error {
 	return nil
 }
