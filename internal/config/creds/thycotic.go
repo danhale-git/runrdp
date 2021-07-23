d7b1fd1ed3f082665c595348ab28f532ef17c5c3
danhale-git <danhale87@gmail.com>
2021-07-23 10:36:19 +0100

    Remove thycotic package.

--- internal/config/creds/thycotic.go
+++ internal/config/creds/thycotic.go
@@ -8,15 +8,18 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/danhale-git/runrdp/internal/thycotic"
 	"github.com/danhale-git/tss-sdk-go/server"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-// ThycoticStruct a struct of type creds.Thycotic.
+// ThycoticStruct returns a struct of type creds.Thycotic.
 func ThycoticStruct() interface{} {
 	return &Thycotic{}
+}
+
+type Secreter interface {
+	Secret(id int) (*server.Secret, error)
 }
 
 // Validate returns an error if a config field is invalid.
@@ -59,14 +62,14 @@ func (t *Thycotic) Retrieve() (string, string, error) {
 		return "", "", fmt.Errorf("creating thycotic server: %s", err)
 	}
 
-	return thycotic.GetCredentials(s, t.SecretID)
+	return GetCredentials(s, t.SecretID)
 }
 
 func credentials() (string, string, error) {
 	stdin := os.Stdin
 	reader := bufio.NewReader(stdin)
 
-	//defer stdin.Close()
+	defer stdin.Close()
 
 	fmt.Print("Enter Thycotic Username: ")
 	username, _ := reader.ReadString('\n')
@@ -82,4 +85,41 @@ func credentials() (string, string, error) {
 	password := string(bytePassword)
 
 	return strings.TrimSpace(username), strings.TrimSpace(password), nil
+}
+
+// GetCredentials calls the Thycotic API via the Go SDK, obtains a secret and attempts to get the Username and Password
+// fields. If either field is not present an error is returned.
+func GetCredentials(s Secreter, secretID int) (string, string, error) {
+	secret, err := s.Secret(secretID)
+
+	if err != nil {
+		return "", "", fmt.Errorf("getting secret '%d' from thycotic: %s", secretID, err)
+	}
+
+	username, err := getField(secret, "Username")
+	if err != nil {
+		return "", "", err
+	}
+
+	password, err := getField(secret, "Password")
+	if err != nil {
+		return "", "", err
+	}
+
+	return username, password, nil
+}
+
+func getField(secret *server.Secret, fieldName string) (string, error) {
+	value, exists := secret.Field(fieldName)
+
+	if !exists {
+		return "", fmt.Errorf(
+			"secret '%s' with template ID %d has no field '%s'",
+			secret.Name,
+			secret.SecretTemplateID,
+			fieldName,
+		)
+	}
+
+	return value, nil
 }
