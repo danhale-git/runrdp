ce39bd4ecbc40f6ca369811b245d5ece0fb3f4af
danhale-git <danhale87@gmail.com>
2021-07-27 08:27:12 +0100

    SecretsManager refactor and test.

--- internal/config/creds/creds_test.go
+++ internal/config/creds/creds_test.go
@@ -16,3 +16,15 @@ func TestThycotic_Validate(t *testing.T) {
 		t.Errorf("no error returned when Thycotic.SecretID is 0")
 	}
 }
+
+func TestSecretsManagerStruct(t *testing.T) {
+	var i interface{} = SecretsManagerStruct()
+
+	if _, ok := i.(*SecretsManager); !ok {
+		t.Errorf("SecretsManagerStruct return value cannot be cast to a SecretsManager struct")
+	}
+}
+
+func TestSecretsManager_Validate(t *testing.T) {
+	// Validate not yet implemented
+}
